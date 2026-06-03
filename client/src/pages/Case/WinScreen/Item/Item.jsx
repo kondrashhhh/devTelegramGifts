62e8125991cc6a0ef4d6c2e9e34815b19f0d38d2
kondrashhhh <hurstay4@gmail.com>
2026-06-03 04:13:17 +0300

    Update Item.jsx

--- client/src/pages/Case/WinScreen/Item/Item.jsx
+++ client/src/pages/Case/WinScreen/Item/Item.jsx
@@ -21,10 +21,10 @@ export const Item = ({ item, index }) => {
                     loop
                     className={styles.itemImage}
                     mode="normal"
-                    src={`${item.image}`}
+                    src={`http://localhost:8080/${item.image}`}
                 />
             )                                : (
-                <img src={item.image} alt={item.name} />
+                <img src={`http://localhost:8080/${item.image}`} alt={item.name} />
             )}
         </div>
         <div className={styles.name}>
