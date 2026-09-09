ff081eb34774e796c99c12e589484654c0e09f19
kondrashhhh <hurstay4@gmail.com>
2026-09-09 23:26:51 +0300

    errorBoundary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,21 +5,21 @@ import { AuthProvider } from './context/AuthContext.jsx'
 import './styles/global.scss'
 import App from './App.jsx'
 
-window.onerror = (msg, src, line, col, err) => {
-  document.body.innerHTML = `
-    <pre style="padding:20px;color:red;white-space:pre-wrap;font-size:14px;">
-${err?.stack || `${msg}\n${src}:${line}:${col}`}
-    </pre>
-  `;
-};
+// window.onerror = (msg, src, line, col, err) => {
+//   document.body.innerHTML = `
+//     <pre style="padding:20px;color:red;white-space:pre-wrap;font-size:14px;">
+// ${err?.stack || `${msg}\n${src}:${line}:${col}`}
+//     </pre>
+//   `;
+// };
 
-window.onunhandledrejection = (e) => {
-  document.body.innerHTML = `
-    <pre style="padding:20px;color:red;white-space:pre-wrap;font-size:14px;">
-${e.reason?.stack || e.reason}
-    </pre>
-  `;
-};
+// window.onunhandledrejection = (e) => {
+//   document.body.innerHTML = `
+//     <pre style="padding:20px;color:red;white-space:pre-wrap;font-size:14px;">
+// ${e.reason?.stack || e.reason}
+//     </pre>
+//   `;
+// };
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
